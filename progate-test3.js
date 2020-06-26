69194a778e659237a788fb49865a4e75825fcddb
virtuecircle <67228212+virtuecircle@users.noreply.github.com>
2020-06-26 19:20:01 +0900

    Add files via upload

---
+++ progate-test3.js
@@ -0,0 +1,7 @@
+$(function() {
+  // 「#change-css」要素に対するclickイベントを作成してください
+  $("#change-css").click(function(){
+    $("#text").css("color","red");
+    $("#text").css("font-size","50px");
+  });
+});
